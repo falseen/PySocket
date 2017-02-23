fa18dfb867d2ab2212e9eb039c7846e23c442ccb
Falseen <hurxyill@163.com>
2017-02-23 11:30:27 +0800

    rename "re_" to "new_" and rename "old" to "real"
    1. add setblocking(True) to "new_send"
    
    2. remove some debug code.

--- proxy/socket.py
+++ proxy/socket.py
@@ -36,6 +36,7 @@ import socket    # 导入真正的socket包
 sys.path.insert(0, path)
 
 import struct
+import binascii
 
 PROXY_TYPE = "socks5"
 PROXY_ADDR = "127.0.0.1"
@@ -43,6 +44,7 @@ PROXY_PORT = 1080
 SOCKS5_REQUEST_DATA = b"\x05\x01\x00"
 
 BUF_SIZE = 32 * 1024
+
 
 def to_bytes(s):
     if bytes != str:
@@ -56,6 +58,7 @@ def to_str(s):
         if type(s) == bytes:
             return s.decode('utf-8')
     return s
+
 
 def pack_addr(address):
     address_str = to_str(address)
@@ -75,84 +78,93 @@ def pack_addr(address):
 
 
 # 动态path类方法
-def re_class_method(_class, method_name, re_method):
+def new_class_method(_class, method_name, new_method):
     method = getattr(_class, method_name)
     info = sys.version_info
     if info[0] >= 3:
         setattr(_class, method_name,
-                types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), _class))
+                types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), _class))
     else:
         setattr(_class, method_name,
-                types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), None, _class))
+                types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), None, _class))
 
 # 动态path实例方法
-def re_self_method(self, method_name, re_method):
+
+
+def new_self_method(self, method_name, new_method):
     method = getattr(self, method_name)
-    setattr(self, method_name, types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), self, self))
+    setattr(self, method_name, types.MethodType(lambda *args, **
+                                                kwds: new_method(method, *args, **kwds), self, self))
 
 # 处理send ，此处为实例方法
-def re_send(old_method, self, *args, **kwds):
+
+
+def new_send(real_method, self, *args, **kwds):
     data = args[0]
     fd = self.fileno()
-    self_id = "%d_%d" %(id(self), fd)
+    self_id = "%d_%d" % (id(self), fd)
     if self.type == 1:
         socks_stage = self._fd_to_self.get(self_id)["socks_stage"]
         if socks_stage != "stream":
-            #print("remote_addrs %s:%d" %(remote_addrs[0], remote_addrs[1]))
+            self.setblocking(True)
             dst_addr, dst_port = self._fd_to_self.get(self_id)["dst_addrs"]
-            #print("send sock %s:%d" %(self.getsockname()[0], self.getsockname()[1]))
-            #print("send peer %s:%d" %(self.getpeername()[0], self.getpeername()[1]))
-            old_send = old_method
-            logging.info("send socks5 hello")
-            old_method(SOCKS5_REQUEST_DATA)
-            time.sleep(0.01)
+            logging.debug("send socks5 hello")
+            real_method(SOCKS5_REQUEST_DATA)
             recv_data = self.recv(BUF_SIZE)
-            if len(recv_data) == 2:
-                socks_type = self.type
-                socks5_cmd = struct.pack(">B", socks_type)
-                dst_port_b = struct.pack('>H', dst_port)
-                dst_addr_b = pack_addr(dst_addr)
-                header_data = b"\x05" +socks5_cmd + b"\x00" + dst_addr_b + dst_port_b
-                logging.info("send socks5 header %s:%d" %(dst_addr, dst_port))
-                old_method(header_data)
-                time.sleep(0.01)
-                recv_data = self.recv(BUF_SIZE)
-                self._fd_to_self.get(self_id)["socks_stage"] = "stream"
-                return_value = old_method(data)
-                return return_value
-    return_value = old_method(*args, **kwds)
-    return return_value   
+            if recv_data == b"\x05\x00":
+                logging.debug("recv socks5 hello %s" % binascii.hexlify(recv_data))
+            else:
+                # TODO
+                logging.debug("not socks5 proxy %s" % binascii.hexlify(recv_data))
+                pass
+            socks_type = self.type
+            socks5_cmd = struct.pack(">B", socks_type)
+            dst_port_b = struct.pack('>H', dst_port)
+            dst_addr_b = pack_addr(dst_addr)
+            header_data = b"\x05" + socks5_cmd + b"\x00" + dst_addr_b + dst_port_b
+            logging.debug("send socks5 header %s:%d" % (dst_addr, dst_port))
+            real_method(header_data)
+            recv_data = self.recv(BUF_SIZE)
+            if recv_data[1] == b"\x00":
+                logging.debug("recv socks5 header %s" % binascii.hexlify(recv_data))
+            else:
+                # TODO
+                logging.debug("connect socks5 proxy faild %s" % binascii.hexlify(recv_data))
+                pass
+            self._fd_to_self.get(self_id)["socks_stage"] = "stream"
+            self.setblocking(False)
+            return_value = real_method(data)
+            return return_value
+    elif self.type == 3:
+        # UDP TODO
+        pass
+    return_value = real_method(*args, **kwds)
+    return return_value
 
-def re_recv(old_method, self, *args, **kwds):
+
+def new_recv(real_method, self, *args, **kwds):
     BUF_SIZE = args[0]
     self_id = id(self)
     fd = self.fileno()
     remote_addrs = self.getsockname()
-    return_value = old_method(BUF_SIZE)
+    return_value = real_method(*args, **kwds)
     return return_value
 
 
 # 处理connect，此处为类方法
-def re_connect(old_method, self, *args, **kwds):
+def new_connect(real_method, self, *args, **kwds):
     dst_addr, dst_port = args[0]
     fd = self.fileno()
-    self_id = "%d_%d" %(id(self), fd)
-    self._fd_to_self.update({self_id : {"dst_addrs":(dst_addr, dst_port), 
-                                    "socks_stage":"init", 
-                                    "data":None,
-                                    "old_send":None
-                                    }
-                            })
-    # print("connect sock %s:%d" %(self.getsockname()[0], self.getsockname()[1]))
-    logging.info("connect dst %s:%d" %(dst_addr, dst_port))
-    re_self_method(self, 'send', re_send)
-    re_self_method(self, 'sendto', re_send)
-    #re_self_method(self, 'recv', re_recv)
+    self_id = "%d_%d" % (id(self), fd)
+    self._fd_to_self.update({self_id: {"dst_addrs": (dst_addr, dst_port),
+                                       "socks_stage": "init",
+                                       }
+                             })
+    logging.info("connect dst %s:%d use proxy %s:%d" % (dst_addr, dst_port, PROXY_ADDR, PROXY_PORT))
+    new_self_method(self, 'send', new_send)
+    # new_self_method(self, 'sendto', new_send)
     PROXY_ADDRS = (PROXY_ADDR, PROXY_PORT)
-    return_value = old_method(self, PROXY_ADDRS, **kwds)
-
-
-
+    return_value = real_method(self, PROXY_ADDRS, **kwds)
 
 setattr(socket.socket, '_fd_to_self', {})
-re_class_method(socket.socket, 'connect', re_connect)
+new_class_method(socket.socket, 'connect', new_connect)
