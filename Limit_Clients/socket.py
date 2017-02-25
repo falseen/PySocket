50734371a9992129d0af8390862da3da4eb2bc39
Falseen <hurxyill@163.com>
2017-02-25 17:49:08 +0800

    update limit_clients use the new scoket
    only suport tcp

--- Limit_Clients/socket.py
+++ Limit_Clients/socket.py
@@ -19,7 +19,7 @@
 # 功能：限制客户端数量（基于ip判断）
 #
 # 使用说明：1.将此文件放在ss根目录中即可生效，不用做其他设置（需要重新运行ss）。
-#          2.修改53、54行的 clean_time 和 ip_numbers 为你想要的数值。
+#          2.修改53、54行的 clean_time 和 client_num 为你想要的数值。
 #          3.如果你的服务器有ipv6,并且你想让ip4和ip6分开限制，则可以设置 only_port 为 False 。
 #
 #
@@ -51,93 +51,61 @@ sys.path.insert(0, path)
 
 
 clean_time = 60        # 设置清理ip的时间间隔，在此时间内无连接的ip会被清理
-ip_numbers = 2         # 设置每个端口的允许通过的ip数量，即设置客户端ip数量
+client_num = 1    # 设置每个端口的允许通过的ip数量，即设置客户端ip数量
 only_port = True       # 设置是否只根据端口判断。如果为 True ，则只根据端口判断。如果为 False ，则会严格的根据 服务端ip+端口进行判断
 
 # 动态path类方法
-def re_class_method(_class, method_name, re_method):
+def new_class_method(_class, method_name, new_method):
     method = getattr(_class, method_name)
     info = sys.version_info
     if info[0] >= 3:
         setattr(_class, method_name,
-                types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), _class))
+                types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), _class))
     else:
         setattr(_class, method_name,
-                types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), None, _class))
+                types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), None, _class))
 
 # 动态path实例方法
-def re_self_method(self, method_name, re_method):
+def new_self_method(self, method_name, new_method):
     method = getattr(self, method_name)
-    setattr(self, method_name, types.MethodType(lambda *args, **kwds: re_method(method, *args, **kwds), self, self))
+    setattr(self, method_name, types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), self, self))
 
 
 # 处理Tcp连接
-def re_accept(old_method, self, *args, **kwds):
+def new_accept(orgin_method, self, *args, **kwds):
+
+    def new_close(orgin_method, socket_self, *args, **kwds):
+        addr = socket_self.getpeername()[0]
+        fd = self_socket.fileno()
+        if len(self._client_list[addr]) == 1:
+            del self._client_list[addr]
+            logging.info("[socket] close and remove the socket %s" % (addr))
+        else:
+            self._client_list[addr].remove(fd)
+            logging.info("[socket] close and remove the fd %s" % (addr))
+        return orgin_method(*args, **kwds)
 
     while True:
-
-        return_value = old_method(self, *args, **kwds)
+        return_value = orgin_method(self, *args, **kwds)
         self_socket = return_value[0]
-        
-        if only_port:
-            server_ip_port = '%s' % self.getsockname()[1]
-        else:
-            server_ip_port = '%s_%s' % (self.getsockname()[0], self.getsockname()[1])
-
-        client_ip = return_value[1][0]
-
-        client_ip_list = [x[0].split('#')[0] for x in self._list_client_ip[server_ip_port]]
-        
-        if len(self._list_client_ip[server_ip_port]) == 0:
-            logging.debug("[re_socket] first add %s" % client_ip)
-            self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
+        client_ip, client_port = return_value[1]
+        fd = self_socket.fileno()
+        if len(self._client_list) < client_num or client_ip in self._client_list:
+            new_self_method(return_value[0], 'close', new_close)
+            logging.info("[socket] add %s:%d" %(client_ip, client_port))
+            if self._client_list.get(client_ip, None) == None:
+                self._client_list.update({client_ip:[]})
+            self._client_list[client_ip].append(fd)
             return return_value
-
-        if client_ip in client_ip_list:
-            logging.debug("[re_socket] update socket in %s" % client_ip)
-            _ip_index = client_ip_list.index(client_ip)
-            self._list_client_ip[server_ip_port][_ip_index][0] = '%s#%s' % (client_ip, time.time())
-            self._list_client_ip[server_ip_port][_ip_index].append(self_socket)
-            return return_value
-
-        else:
-            if len(self._list_client_ip[server_ip_port]) < ip_numbers:
-                logging.debug("[re_socket] add %s" % client_ip)
-                self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
-                return return_value
-
-            for x in [x for x in self._list_client_ip[server_ip_port]]:
-                is_closed = True
-                if time.time() - float(x[0].split('#')[1]) > clean_time:
-
-                    for y in x[1:]:
-                        try:
-                            y.getpeername()     # 判断连接是否关闭
-                            is_closed = False
-                            break
-                        except:                # 如果抛出异常，则说明连接已经关闭，这时可以关闭套接字
-                            logging.debug("[re_socket] close and remove the time out socket 1/%s" % (len(x[1:])))
-                            x.remove(y)
-
-                    if not is_closed:
-                        logging.debug('[re_socket] the %s still exists and update last_time' % str(x[1].getpeername()[0]))
-                        _ip_index = client_ip_list.index(x[0].split('#')[0])
-                        self._list_client_ip[server_ip_port][_ip_index][0] = '%s#%s' % (x[0].split('#')[0], time.time())
-
-                    else:
-                        logging.info("[re_socket] remove time out ip and add new ip %s" % client_ip )
-                        self._list_client_ip[server_ip_port].remove(x)
-                        self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
-                        return return_value
-
-        if int(time.time()) % 5 == 0:
-            logging.debug("[re_socket] the port %s client more then the %s" % (server_ip_port, ip_numbers))
+        server_addr, server_port = self.getsockname()
+        logging.info("[socket] the %s:%d client more then the %s" % (server_addr, server_port, client_num))
+        self_socket.close()     
 
 # 处理Udp连接
-def re_recvfrom(old_method, self, *args, **kwds):
+def new_recvfrom(orgin_method, self, *args, **kwds):
 
     while True:
-        return_value = old_method(*args, **kwds)
+        return_value = orgin_method(*args, **kwds)
         self_socket = ''
         if only_port:
             server_ip_port = '%s' % self.getsockname()[1]
@@ -147,18 +115,18 @@ def re_recvfrom(old_method, self, *args, **kwds):
         client_ip_list = [x[0].split('#')[0] for x in self._list_client_ip[server_ip_port]]
 
         if len(self._list_client_ip[server_ip_port]) == 0:
-            logging.debug("[re_socket] first add %s" % client_ip)
+            logging.debug("[socket] first add %s" % client_ip)
             self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
             return return_value
 
         if client_ip in client_ip_list:
-            logging.debug("[re_socket] update socket in %s" % client_ip)
+            logging.debug("[socket] update socket in %s" % client_ip)
             _ip_index = client_ip_list.index(client_ip)
             self._list_client_ip[server_ip_port][_ip_index][0] = '%s#%s' % (client_ip, time.time())
             return return_value
         else:
-            if len(self._list_client_ip[server_ip_port]) < ip_numbers:
-                logging.debug("[re_socket] add %s" % client_ip)
+            if len(self._list_client_ip[server_ip_port]) < client_num:
+                logging.debug("[socket] add %s" % client_ip)
                 self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
                 return return_value
 
@@ -172,38 +140,53 @@ def re_recvfrom(old_method, self, *args, **kwds):
                             is_closed = False
                             break
                         except:                # 如果抛出异常，则说明连接已经关闭，这时可以关闭套接字
-                            logging.debug("[re_socket] close and remove the time out socket 1/%s" % (len(x[1:])))
+                            logging.debug("[socket] close and remove the time out socket 1/%s" % (len(x[1:])))
                             x.remove(y)
 
                     if not is_closed:
-                        logging.debug('[re_socket] the %s still exists and update last_time' % str(x[1].getpeername()[0]))
+                        logging.debug('[socket] the %s still exists and update last_time' % str(x[1].getpeername()[0]))
                         _ip_index = client_ip_list.index(x[0].split('#')[0])
                         self._list_client_ip[server_ip_port][_ip_index][0] = '%s#%s' % (x[0].split('#')[0], time.time())
 
                     else:
-                        logging.info("[re_socket] remove time out ip and add new ip %s" % client_ip )
+                        logging.info("[socket] remove time out ip and add new ip %s" % client_ip )
                         self._list_client_ip[server_ip_port].remove(x)
                         self._list_client_ip[server_ip_port].append(['%s#%s' % (client_ip, time.time()), self_socket])
                         return return_value
 
         if int(time.time()) % 5 == 0:
-            logging.debug("[re_socket] the port %s client more then the %s" % (server_ip_port, ip_numbers))
+            logging.debug("[socket] the port %s client more then the %s" % (server_ip_port, client_num))
         new_tuple = [b'', return_value[1]]
         return_value = tuple(new_tuple)
         return return_value
 
 
-def re_bind(old_method, self, *args, **kwds):
-    if only_port:
-        port = '%s' % args[0][1]
-    else:
-        port = '%s_%s' % (args[0][0], args[0][1])
-    self._list_client_ip[port] = []
-    re_self_method(self, 'recvfrom', re_recvfrom)
-    old_method(self, *args, **kwds)
+def new_bind(orgin_method, self, *args, **kwds):
+    #new_self_method(self, 'recvfrom', new_recvfrom)
+    orgin_method(self, *args, **kwds)
 
 
-setattr(socket.socket, '_list_client_ip', {})
-re_class_method(socket.socket, 'bind', re_bind)
-re_class_method(socket.socket, 'accept', re_accept)
+class new_socket(socket.socket):
 
+    def __init__(self, *args, **kwds):
+        super(new_socket, self).__init__(*args, **kwds)
+        # new_self_method(self, 'sendto', new_sendto)
+        # new_self_method(self, 'recvfrom', new_recvfrom)
+        self._client_list = {}
+
+    def close(self, *args, **kwds):
+        super(new_socket, self).__init__(*args, **kwds)
+
+
+class new_client_socket(socket.socket):
+
+    def __init__(self, *args, **kwds):
+        super(new_client_socket, self).__init__(*args, **kwds)
+
+    def close(self ,*args, **kwds):
+        super(new_client_socket, self).__init__(*args, **kwds)
+
+new_class_method(socket.socket, 'bind', new_bind)
+new_class_method(socket.socket, 'accept', new_accept)
+socket.socket = new_socket
+socket._socketobject = new_client_socket
