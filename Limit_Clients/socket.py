88e1767ec7d64e410df35887a8126d8e021bc4ee
Falseen <hurxyill@163.com>
2017-02-26 18:48:24 +0800

    mv new_close to new_class and fix some bug

--- Limit_Clients/socket.py
+++ Limit_Clients/socket.py
@@ -48,12 +48,13 @@ sys.path.insert(0, path)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-set_close_timeout = False   # 是否清理指定时间内无数据收发的连接，仅对TCP有效，一般情况下socket会自动关闭正常或异常断开的连接。
-                            #   如果为True则根据下面的超时时间进行清理，如果为 False 则根据socket的超时时间来清理。
+set_close_timeout = True   # 是否清理指定时间内无数据收发的连接，仅对TCP有效，socket会自动关闭正常或异常断开的连接, 所以一般不用设置。
+                            #   如果为True则根据下面的超时时间进行清理，如果为 False 则根据socket的超时时间来清理(5分钟左右)。
 
-recv_timeout = 4000         # 设置清理连接的超时时间，单位毫秒。在此时间内无连接或无数据 接收 的连接会被清理。
-                            # 主要针对 udp，如果 set_close_timeout 为True的话也对tcp生效，但对于tcp不是强制性的。
+recv_timeout = 1000         # 设置 tcp 清理连接的超时时间，单位毫秒（1000毫秒等于一分钟）。
+                            #    在此时间内无连接或无数据 接收 的连接会被清理。只针对 tcp。
 
+recvfrom_timeout = 60       # 设置 udp 清理连接的超时时间，单位秒。在此时间内无连接或无数据 接收 的连接会被清理。只针对 udp。
 limit_clients_num = 1       # 设置每个端口的允许通过的ip数量，即客户端的ip数量
 only_port = True            # 设置是否只根据端口判断。如果为 True ，则只根据端口判断。如果为 False ，则会严格的根据服务端ip+端口进行判断
 
@@ -79,25 +80,6 @@ def new_self_method(self, method_name, new_method):
     setattr(self, method_name, types.MethodType(lambda *args, **kwds: new_method(method, *args, **kwds), self, self))
 
 
-# 自定义 close 方法，让其在关闭的时候从列表中清理掉自身的 socket 或 ip。
-def new_close(orgin_method, self_socket, *args, **kwds):
-    addr, port = self_socket.getpeername()
-    self = self_socket
-    server_addrs = self._server_addrs
-    client_list = self._all_client_list[server_addrs]
-    if client_list.get(addr, None) != None:
-        last_up_time = client_list[addr]["last_up_time"]
-        if client_list[addr]["client_num"] <= 1 and time.time() - last_up_time > recv_timeout:
-            del client_list[addr]
-            logging.info("[socket] remove the client %s" % (addr))
-        else:
-            client_list[addr]["client_num"] -= 1
-            # logging.debug("[socket] close the client socket %s:%d" % (addr, port))
-        self._all_client_list[server_addrs].update(client_list)
-
-    return orgin_method(*args, **kwds)
-
-
 # 处理Tcp连接
 def new_accept(orgin_method, self, *args, **kwds):
 
@@ -109,7 +91,7 @@ def new_accept(orgin_method, self, *args, **kwds):
         client_list = self._all_client_list.get(server_addrs, {})
         if len(client_list) < limit_clients_num or client_ip in client_list:
             self_socket._server_addrs = self._server_addrs
-            new_self_method(self_socket, 'close', new_close)
+            self_socket.close = self_socket.new_close
             # logging.debug("[socket] add %s:%d" %(client_ip, client_port))
             if client_list.get(client_ip, None) == None:
                 client_list.update({client_ip : {"client_num":0, "last_up_time":0}})
@@ -121,16 +103,18 @@ def new_accept(orgin_method, self, *args, **kwds):
         else:
             for k,v in self._all_client_list[server_addrs].copy().items():
                 last_up_time = v["last_up_time"]
-                if time.time() - last_up_time > recv_timeout and v["client_num"] <= 1:
-                    del self._all_client_list[server_addrs][k]
+                if time.time() - last_up_time > recvfrom_timeout and v["client_num"] < 1:
+                    logging.info("[socket] remove the client %s" % (k))
+                    del client_list[k]
                     if set_close_timeout:
                         self_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, recv_timeout)
+                    self._all_client_list[server_addrs].update(client_list)
                     self_socket._server_addrs = self._server_addrs
-                    new_self_method(self_socket, 'close', new_close)
+                    self_socket.close = self_socket.new_close
                     return return_value
         if time.time() - self.last_log_time > 10:
             self.last_log_time = time.time()
-            logging.error("[socket] the %s client more then the %d" % (server_addrs, limit_clients_num))
+            logging.error("[socket] the server_addrs %s client more then the %d" % (server_addrs, limit_clients_num))
         self_socket.close()
 
 
@@ -153,30 +137,36 @@ def new_recvfrom(orgin_method, self, *args, **kwds):
         else:
             for k,v in self._all_client_list[server_addrs].copy().items():
                 last_up_time = v["last_up_time"]
-                if time.time() - last_up_time > recv_timeout and v["client_num"] <= 1:
-                    del self._all_client_list[server_addrs][k]
-                    # logging.debug("[socket] update last_up_time for %s" % client_ip)
+                if time.time() - last_up_time > recvfrom_timeout and v["client_num"] < 1:
+                    logging.info("[socket] remove the client %s" % (k))
+                    del client_list[k]
+                    # logging.debug("[socket] add %s:%d" %(client_ip, client_port))
                     client_list[client_ip]["last_up_time"] = time.time()
                     self._all_client_list[server_addrs].update(client_list)
                     return return_value
 
         if time.time() - self.last_log_time > 10:
             self.last_log_time = time.time()
-            logging.error("[socket] the port %s client more then the %d" % (server_addrs, client_num))
+            logging.error("[socket] the server_addrs %s client more then the %d" % (server_addrs, limit_clients_num))
         new_tuple = [b'', return_value[1]]
         return_value = tuple(new_tuple)
         return return_value
 
 
 def new_bind(orgin_method, self, *args, **kwds):
-    if only_port:
-        server_addrs = '*:%s' % args[0][1]
-    else:
-        server_addrs = '%s:%s' % (args[0][0], args[0][1])
-    self._server_addrs = server_addrs
-    self._all_client_list.update({server_addrs:{}})
-    new_self_method(self, 'recvfrom', new_recvfrom)
+    
+    # 如果是udp的绑定，则绑定地址一般是0。那这个socket就一定不是和客户端通信的。
+    # 此处主要是考虑到shadowsocks服务端在访问非客户端的时候会对udp进行绑定。
+    if args[0][1] != 0:
+        if only_port:
+            server_addrs = '*:%s' % args[0][1]
+        else:
+            server_addrs = '%s:%s' % (args[0][0], args[0][1])
+        self._server_addrs = server_addrs
+        self._all_client_list.update({server_addrs:{}})
+        new_self_method(self, 'recvfrom', new_recvfrom)
     orgin_method(self, *args, **kwds)
+    
 
 
 # 自定义 socket 类，可以自定义实例方法或属性。
@@ -195,6 +185,21 @@ class new_client_socket(socket.socket):
     def close(self ,*args, **kwds):
         super(new_client_socket, self).__init__(*args, **kwds)
 
+    # 自定义 close 方法，让其在关闭的时候从列表中清理掉自身的 socket 或 ip。
+    def new_close(self, *args, **kwds):
+        addr, port = self.getpeername()
+        server_addrs = self._server_addrs
+        client_list = self._all_client_list[server_addrs]
+        if client_list.get(addr, None) != None:
+            last_up_time = client_list[addr]["last_up_time"]
+            if client_list[addr]["client_num"] <= 1 and time.time() - last_up_time > recvfrom_timeout:
+                del client_list[addr]
+                logging.info("[socket] remove the client %s" % (addr))
+            else:
+                client_list[addr]["client_num"] -= 1
+                # logging.debug("[socket] close the client socket %s:%d" % (addr, port))
+            self._all_client_list[server_addrs].update(client_list)
+        return super(new_client_socket, self).__init__(*args, **kwds)
 
 
 # 添加类属性， 此属性是全局的，所有socket对象都共享此属性。
@@ -203,5 +208,5 @@ setattr(socket.socket, 'last_log_time', 0)
 
 new_class_method(socket.socket, 'bind', new_bind)
 new_class_method(socket.socket, 'accept', new_accept)
-socket._socketobject = new_client_socket
 socket.socket = new_socket
+socket._socketobject = new_client_socket
