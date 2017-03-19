b90407945d976d466426a445e317dde728c0628a
Falseen <hurxyill@163.com>
2017-03-19 14:37:19 +0800

    try fix some error for limit_clients

--- Limit_Clients/socket.py
+++ Limit_Clients/socket.py
@@ -93,7 +93,9 @@ def new_accept(orgin_method, self, *args, **kwds):
         client_ip, client_port = return_value[1]
         server_addrs = self._server_addrs
         client_list = self._all_client_list.get(server_addrs, {})
-        if len(client_list) < limit_clients_num or client_ip in client_list:
+        if client_ip in client_list:
+            return return_value
+        elif len(client_list) < limit_clients_num:
             self_socket._server_addrs = self._server_addrs
             self_socket.close = self_socket.new_close
             logging.debug("[socket] add %s:%d" %(client_ip, client_port))
